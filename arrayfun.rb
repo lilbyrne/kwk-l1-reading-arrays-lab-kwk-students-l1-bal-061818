0239e181bafc24401c088511cc002c2e54273567
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-20 02:33:57 +0000

    Done.

---
+++ arrayfun.rb
@@ -0,0 +1,12 @@
+ students = ["allison", "lily", "hadja", "siobhan", "anu"]
+
+
+ students[2] = "Cassidy"
+
+ students[4] = "Megan"
+
+students.push("Grace")
+
+
+students.push("Shay")
+puts students

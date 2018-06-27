a4f969929822d5324535afc7403ecd2f2711fa28
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-27 02:03:42 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,3 +1,43 @@
+# STUDENT_NAMES = [
+#   "Adele",
+#   "Beyoncé",
+#   "Cardi B",
+#   "Lady Gaga",
+#   "Nicki Minaj",
+#   "Rihanna"
+# ]
+
+# def first_student_by_index
+#   puts STUDENT_NAMES[0]
+# end
+
+# def fourth_student_by_index
+#   puts STUDENT_NAMES[3]
+# end
+# fourth_student_by_index
+
+# def last_student_by_index
+#   puts STUDENT_NAMES[5]
+# end
+# last_student_by_index
+
+# def first_student_by_method
+#   puts STUDENT_NAMES.first
+# end
+# first_student_by_method
+
+# def last_student_by_method
+#   puts STUDENT_NAMES.last
+# end
+# last_student_by_method
+
+# def first_second_and_third_students
+#   puts STUDENT_NAMES[0]
+#   puts STUDENT_NAMES[1]
+#   puts STUDENT_NAMES[2]
+# end
+# first_second_and_third_students
+
 STUDENT_NAMES = [
   "Adele",
   "Beyoncé",
@@ -8,32 +48,31 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  puts STUDENT_NAMES[0]
+  # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
-  puts STUDENT_NAMES[3]
+  # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[3]
 end
-fourth_student_by_index
 
 def last_student_by_index
-  puts STUDENT_NAMES[5]
+  # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[5]
 end
-last_student_by_index
 
 def first_student_by_method
-  puts STUDENT_NAMES.first
+  # Write a solution that returns the first student using the built in .first method
+  STUDENT_NAMES.first
 end
-first_student_by_method
 
 def last_student_by_method
-  puts STUDENT_NAMES.last
+  # Write a solution that returns the first student using the built in .last method
+  STUDENT_NAMES.last
 end
-last_student_by_method
 
 def first_second_and_third_students
-  puts STUDENT_NAMES[0]
-  puts STUDENT_NAMES[1]
-  puts STUDENT_NAMES[2]
+  # Write a solution that returns the first, second and third students
+  STUDENT_NAMES[0..2]
 end
-first_second_and_third_students
